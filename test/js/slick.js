65ce05478e9e4992b1e4d30688d54fd94ecc1634
kostya <tkt13b.kulchikovskij@gmail.com>
2020-02-06 09:25:26 +0200

    dssd

---
+++ test/js/slick.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+	$('.first-slider__slider').slick({
+		autoplay: true,
+		speed: 300,
+		dots: true,
+		infinite: true,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		arrows: false
+	});
+});
+
+$(document).ready(function(){
+	$('.second-slider__slider').slick({
+		autoplay: true,
+		speed: 500,
+		dots: true,
+		infinite: true,
+		arrows: false,
+		infinite: true,
+		fade: true,
+		cssEase: 'linear',
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		centerMode: true,
+		centerPadding: '50px'
+	});
+});
+
+$(document).ready(function(){
+	$('.third-slider__slider').slick({
+		autoplay: true,
+		speed: 500,
+		dots: false,
+		arrows: false,
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+	});
+});
+
+$('.first-slider__next-arrow').on('click', function() {
+  $('.first-slider__slider').slick('slickNext');
+});
+$('.first-slider__prev-arrow').on('click', function() {
+  $('.first-slider__slider').slick('slickPrev');
+});
